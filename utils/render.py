46ce0e6059e33a680d7018f2bf4c24500509d754
PatrickLisiecki <impatricklisiecki@gmail.com>
2025-02-04 19:32:42 -0500

    create utils directory

---
+++ utils/render.py
@@ -0,0 +1,13 @@
+def layout(text):
+    display_list = []
+    HSTEP, VSTEP = 13, 18
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+
+    return display_list
